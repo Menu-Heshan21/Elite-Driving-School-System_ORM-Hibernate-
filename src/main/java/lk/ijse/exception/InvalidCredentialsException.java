449aeca4d740ddefa1b3fb876d261f27c5d49752
mhesh <iammenushaheshan22@gmail.com>
2025-09-23 10:49:52 +0530

    invalid credentials exception

---
+++ src/main/java/lk/ijse/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package lk.ijse.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
